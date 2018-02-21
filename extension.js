7feddc230788cb27eb58b5a8488382342b291393
Chalk <chalkpe@gmail.com>
2018-02-21 19:19:52 +0900

    basic structure

--- extension.js
+++ extension.js
@@ -1,87 +1,65 @@
-const St = imports.gi.St
-const Clutter = imports.gi.Clutter
+/* global imports */
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^(init|enable|disable)$" }] */
 
 const Lang = imports.lang
-const Main = imports.ui.main
 const Mainloop = imports.mainloop
-const Tweener = imports.ui.tweener
+
+// gi
+const St = imports.gi.St
+const Gtk = imports.gi.Gtk
+const Clutter = imports.gi.Clutter
+
+// ui
+const Main = imports.ui.main
+const PanelMenu = imports.ui.panelMenu
+const PopupMenu = imports.ui.popupMenu
 
 let loopId = null
-let text, button, metadata, counter
+let counter = null
 
-function _hideHello() {
-  Main.uiGroup.remove_actor(text)
-  text = null
-}
+const TweetCounter = Lang.Class({
+  Name: 'TweetCounter',
+  Extends: PanelMenu.Button,
 
-function _showHello(zzz) {
-  if (!text) {
-    text = new St.Label({
-      text: zzz + ' zzz?',
-      style_class: 'helloworld-label'
-    })
+  _init: function () {
+    this.parent(null, 'TweetCounter')
 
-    Main.uiGroup.add_actor(text)
+    this.box = new St.BoxLayout()
+    this.actor.add_actor(this.box)
+
+    this.box.add_actor(this.icon = new St.Icon({
+      icon_name: 'tweet-larry',
+      style_class: 'system-status-icon'
+    }))
+
+    this.box.add_actor(this.label = new St.Label({
+      text: ':thinking:',
+      y_align: Clutter.ActorAlign.CENTER
+    }))
+
+    this.menuItem = new PopupMenu.PopupMenuItem('Tweet Counter', {})
+    this.menu.addMenuItem(this.menuItem)
+  },
+
+  // TODO: Update *real* information
+  _refresh: function () {
+    this.label.set_text(Math.floor(Math.random() * 1000) + 'K')
+    return true
   }
+})
 
-  let monitor = Main.layoutManager.primaryMonitor
-  text.set_position(
-    monitor.x + Math.floor(monitor.width / 2 - text.width / 2),
-    monitor.y + Math.floor(monitor.height / 2 - text.height / 2)
-  )
-
-  text.opacity = 255
-  Tweener.addTween(text, {
-    time: 2,
-    opacity: 0,
-    transition: 'easeOutQuad',
-    onComplete: _hideHello
-  })
+function init (meta) {
+  const Theme = Gtk.IconTheme.get_default()
+  Theme.append_search_path(meta.path + '/icons')
+  if (counter === null) counter = new TweetCounter()
 }
 
-function init(meta) {
-  let theme = imports.gi.Gtk.IconTheme.get_default()
-  theme.append_search_path(meta.path + '/icons')
-
-  button = new St.Bin({
-    style_class: 'panel-button',
-    reactive: true,
-    can_focus: true,
-    x_fill: true,
-    y_fill: false,
-    track_hover: true
-  })
-
-  let counter = new St.Label({
-    y_align: Clutter.ActorAlign.CENTER
-  })
-
-  let icon = new St.Icon({
-    icon_name: 'tweet-larry',
-    style_class: 'system-status-icon'
-  })
-
-  let box = new St.BoxLayout()
-  box.add_actor(icon)
-  box.add_actor(counter)
-
-  button.set_child(box)
-  button.connect('button-press-event', _showHello)
+function enable () {
+  Main.panel.addToStatusArea('tweet-counter', counter, 0)
+  loopId = Mainloop.timeout_add_seconds(1, () => counter._refresh())
 }
 
-function enable() {
-  Main.panel._rightBox.insert_child_at_index(button, 0)
-  loopId = Mainloop.timeout_add_seconds(3, onRefresh)
-}
-
-function onRefresh () {
-  _showHello(Math.random())
-  counter.set_text(Math.floor(Math.random() * 1000) + 'K')
-
-  return true
-}
-
-function disable() {
-  Main.panel._rightBox.remove_child(button)
-  Mainloop.source_remove(loopId)
+function disable () {
+  if (counter !== null) counter.destroy()
+  if (loopId !== null) Mainloop.source_remove(loopId)
 }
