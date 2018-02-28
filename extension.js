a72db3807900a7bb418708bb7adecafbdd76fd1d
Chalk <chalkpe@gmail.com>
2018-03-01 01:03:10 +0900

    add option for number abbreviation

--- extension.js
+++ extension.js
@@ -20,21 +20,24 @@ const Util = imports.misc.util
 const ExtensionUtils = imports.misc.extensionUtils
 const Me = ExtensionUtils.getCurrentExtension()
 
-let schema = null
 let loopId = null
 let counter = null
-let session = new Soup.SessionAsync()
+let settings = null
 
+// networking
 const url = 'https://twitter.com/'
+const session = new Soup.SessionAsync()
+const cookies = [Soup.Cookie.parse('lang=en-gb;', new Soup.URI(url))]
 const regex = /ProfileNav-item--tweets[\s\S]*?data-count=(\d+)[\s\S]*?data-is-compact="(.*?)"[\s\S]*?>[\s\S]*?(.*)[\s\S]*?<\/span>/g
 
+// widget
 const TweetCounter = Lang.Class({
   Name: 'TweetCounter',
   Extends: PanelMenu.Button,
 
   _init: function () {
     this.parent(null, 'TweetCounter')
-    const id = '@' + schema.get_string('username')
+    this.counts = [':thinking_face:', ':thinking:']
 
     this.box = new St.BoxLayout()
     this.actor.add_actor(this.box)
@@ -44,25 +47,43 @@ const TweetCounter = Lang.Class({
       style_class: 'system-status-icon'
     }))
 
-    this.box.add_actor(this.label = new St.Label({
-      text: ':thinking:',
+    this.box.add_actor(this.countLabel = new St.Label({
       y_align: Clutter.ActorAlign.CENTER
     }))
 
-    this.menuItem = new PopupMenu.PopupMenuItem('Twitter Settings', {})
-    this.menuItem.connect('activate', () => Util.spawn(['gnome-shell-extension-prefs', Me.metadata.uuid]))
+    this.profileMenu = new PopupMenu.PopupMenuItem('@thinking', {})
+    this.settingsMenu = new PopupMenu.PopupMenuItem('Twitter Settings', {})
 
-    this.menu.addMenuItem(new PopupMenu.PopupMenuItem(id, {}))
-    this.menu.addMenuItem(this.menuItem)
+    this.profileMenu.connect('activate', () => Util.spawn(['xdg-open', url + this.username]))
+    this.settingsMenu.connect('activate', () => Util.spawn(['gnome-shell-extension-prefs', Me.metadata.uuid]))
+
+    this.menu.addMenuItem(this.profileMenu)
+    this.menu.addMenuItem(this.settingsMenu)
+
+    this._setup('compact')
+    this._setup('username')
+  },
+
+  _setup: function (key) {
+    if (key === 'username') {
+      this.username = settings.get_string('username')
+      this.profileMenu.label.set_text('@' + this.username)
+    } else if (key === 'compact') {
+      this.compact = settings.get_boolean('compact')
+      this.countLabel.set_text(this.counts[+this.compact])
+    }
   },
 
   _refresh: function () {
-    const username = schema.get_string('username')
-    const req = Soup.Message.new('GET', url + username)
+    const req = Soup.Message.new('GET', url + this.username)
+    Soup.cookies_to_request(cookies, req)
 
     session.queue_message(req, () => {
       const m = regex.exec(req.response_body.data)
-      if (m !== null) this.label.set_text(m[1])
+      if (m === null) return // this.countLabel.set_text(':cry:')
+
+      this.counts = [m[1], m[m[2] === 'true' ? 3 : 1]]
+      this.countLabel.set_text(this.counts[+this.compact])
     })
 
     return true
@@ -72,7 +93,9 @@ const TweetCounter = Lang.Class({
 function init (meta) {
   const Theme = Gtk.IconTheme.get_default()
   Theme.append_search_path(meta.path + '/icons')
-  schema = Me.imports.convenience.getSettings()
+
+  settings = Me.imports.convenience.getSettings()
+  settings.connect('changed', (settings, key) => counter._setup(key))
 }
 
 function enable () {
--- prefs.js
+++ prefs.js
@@ -2,46 +2,58 @@
   ["error", { "varsIgnorePattern": "^(init|buildPrefsWidget)$" }] */
 
 const Gtk = imports.gi.Gtk
-const GdkPixbuf = imports.gi.GdkPixbuf
 const ExtensionUtils = imports.misc.extensionUtils
+const createPixbuf = imports.gi.GdkPixbuf.Pixbuf.new_from_file_at_scale
 
-let atPath = null
-let schema = null
+let path = null
+let layout = null
+let settings = null
 
 function init () {
   const Me = ExtensionUtils.getCurrentExtension()
   const Convenience = Me.imports.convenience
 
-  schema = Convenience.getSettings()
-  atPath = Me.path + '/icons/at.svg'
+  path = Me.path
+  settings = Convenience.getSettings()
 }
 
-function buildPrefsWidget () {
+function addSetting ({ title, icon, control }) {
   const label = new Gtk.Label()
-  label.set_markup('<b>Your Twitter username</b>')
+  label.set_markup(`<b>${title}</b>`)
   label.set_alignment(0, 0.5) // x: left, y: centre
+  layout.add(label)
+
+  if (!icon) return layout.add(control)
 
   const img = new Gtk.Image()
   img.set_margin_right(5)
-  img.set_from_pixbuf(GdkPixbuf.Pixbuf.new_from_file_at_scale(atPath, 20, 20, true))
-
-  const input = new Gtk.Entry({ text: schema.get_string('username') })
-  input.set_sensitive(true)
-  input.connect('changed', widget => schema.set_string('username', widget.get_text()))
+  img.set_from_pixbuf(createPixbuf(`${path}/icons/${icon}.svg`, 20, 20, true))
 
   const field = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL })
   field.add(img)
-  field.add(input)
+  field.add(control)
+  layout.add(field)
+}
 
-  const layout = new Gtk.Box({
+function buildPrefsWidget () {
+  layout = new Gtk.Box({
     orientation: Gtk.Orientation.VERTICAL,
     spacing: 10,
     border_width: 10
   })
 
-  layout.add(label)
-  layout.add(field)
-  layout.show_all()
+  const username = new Gtk.Entry({ text: settings.get_string('username') })
+  const compact = new Gtk.CheckButton({
+    active: settings.get_boolean('compact'),
+    label: 'Abbreviate large number. (81800 -> 81.8K)'
+  })
 
+  username.connect('changed', widget => settings.set_string('username', widget.get_text()))
+  compact.connect('toggled', widget => settings.set_boolean('compact', widget.get_active()))
+
+  addSetting({ control: username, title: 'Your Twitter username', icon: 'at' })
+  addSetting({ control: compact, title: 'Compact count' })
+
+  layout.show_all()
   return layout
 }
